c314c6a0080018d32da6f72573e7622a4f710e44
ibarretorey <ibarretorey@gmail.com>
2020-06-09 20:16:28 -0300

    changes awx config

--- roles/run_awx/templates/environment.sh
+++ roles/run_awx/templates/environment.sh
@@ -10,3 +10,9 @@ RABBITMQ_HOST={{ rabbitmq_host }}
 RABBITMQ_PORT={{ rabbitmq_port }}
 AWX_ADMIN_USER={{ awx_admin_username }}
 AWX_ADMIN_PASSWORD={{ awx_admin_password | quote }}
+{% if env == 'dev' %}
+        TOWER_URL_BASE= u"http://{{ public_domain }}"
+{% else %}
+        TOWER_URL_BASE= u"https://{{ public_domain }}"
+{% endif %}
+INTERNAL_API_URL='http://awxweb:8052'
